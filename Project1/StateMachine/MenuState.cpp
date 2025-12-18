bfcab6d2584f72ce841548ef6fde3094aec51507
Miki18 <mikiadam24@wp.pl>
2025-12-18 17:21:52 +0100

    1.2.2a Changed version in Readme and Main menu Removed test profile

--- Project1/StateMachine/MenuState.cpp
+++ Project1/StateMachine/MenuState.cpp
@@ -157,7 +157,7 @@ void MenuState::MainMenuScreen()
 	int VersionTextOffset = 25;
 	ImGui::SetNextWindowPos(ImVec2(0,ScreenSize[1] - VersionTextOffset));
 	ImGui::Begin("Version", nullptr, ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar);
-	ImGui::Text("Version 1.2.1b");
+	ImGui::Text("Version 1.2.2a");
 	ImGui::End();
 }
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ZombieTowerDefense
 
-version 1.2.1b
+version 1.2.2a
 
